406b4863067c923f63a9c392c77ff8a2af8084da
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-03-21 15:16:49 +0100

    Update searchresults.js

--- src/components/searchresults.js
+++ src/components/searchresults.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import BookCard from './BookCard';
 
+const SearchResults = ({ results }) => {
+  return (
+    <div className="search-results">
+      {results.map((book, index) => (
+        <BookCard key={index} book={book} />
+      ))}
+    </div>
+  );
+};
+
+export default SearchResults;
