250beeab4c11103d9c472463dbd7f14d21b3812b
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-03-21 16:40:39 +0100

    Update MovieCard.js

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,8 +5,8 @@ const MovieCard = ({ book }) => {
   return (
     <div className="book">
       <h2>{book.title}</h2>
-      <p>Author: {book.author_name ? book.author_name.join(', ') : 'Ian Fleming'}</p>
-      <p>Year: {book.first_publish_year}</p>
+      <p>Forfatter: {book.author_name ? book.author_name.join(', ') : 'Ian Fleming'}</p>
+      <p>År: {book.first_publish_year}</p>
       {book.cover_i && <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`} alt="Book Cover" />}
       {/* Vis bilde hvis cover_i-feltet er tilgjengelig */}
     </div>
