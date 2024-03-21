49b4b3b5da067721f59c7a7522848dc6ad48b8b9
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-03-21 15:08:41 +0100

    Update searchbar.js

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,1 +1,28 @@
+import React, { useState } from 'react';
 
+const SearchBar = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onSearch(searchTerm);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Search for James Bond books..."
+        value={searchTerm}
+        onChange={handleInputChange}
+      />
+      <button type="submit">Search</button>
+    </form>
+  );
+};
+
+export default SearchBar;
