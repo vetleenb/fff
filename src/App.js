559bc4952a956a3b2f26ba891029b66789d7e129
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-03-21 15:11:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import SearchBar from '.components/searchbar';
 
 const App = () => {
   const [books, setBooks] = useState([]);
