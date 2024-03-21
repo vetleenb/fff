b32e294c5a2e2067d3cdb6aa8ccd38dd860857bd
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-03-21 15:16:23 +0100

    Update moviecard.js

--- src/components/moviecard.js
+++ src/components/moviecard.js
@@ -1,1 +1,14 @@
+import React from 'react';
 
+const BookCard = ({ book }) => {
+  return (
+    <div className="book">
+      <h2>{book.title}</h2>
+      <p>Author: {book.author}</p>
+      <p>Year: {book.year}</p>
+      {/* Add more book details as needed */}
+    </div>
+  );
+};
+
+export default BookCard;
