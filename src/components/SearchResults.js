6045d316cde06630717a5c780ce5d365e0902159
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-03-21 16:38:13 +0100

    Update SearchResults.js

--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -18,10 +18,10 @@ const SearchResults = ({ results }) => {
       {results.map((result, index) => (
         <div key={index} className="result">
           <h2>{result.title}</h2>
-          <p>Published Year: {result.first_publish_year}</p>
-          <p>Author: {result.author_name ? result.author_name.join(', ') : 'Unknown'}</p>
-          <p>Average Rating: {result.average_rating || 'N/A'}</p>
-          <a href={`https://www.amazon.com/s?k=${result.amazon_id}`} target="_blank" rel="noopener noreferrer">Search on Amazon</a>
+          <p>Utgivelsesår: {result.first_publish_year}</p>
+          <p>Forfatter: {result.author_name ? result.author_name.join(', ') : 'Unknown'}</p>
+          <p>Gjennomsnittlig rating: {result.average_rating || 'N/A'}</p>
+          <a href={`https://www.amazon.com/s?k=${result.amazon_id}`} target="_blank" rel="noopener noreferrer">Søk på Amazone</a>
         </div>
       ))}
     </div>
