dc0feb01953e7a748d0ac633dc6859ddc36fda6f
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-04-03 15:31:49 +0200

    Update BookCard.js

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -8,7 +8,6 @@ const BookCard = ({ book }) => {
       <p>Forfatter: {book.author_name ? book.author_name.join(', ') : 'Ian Fleming'}</p>
       <p>År: {book.first_publish_year}</p>
 {book.cover_i && <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`} alt="Book Cover" />}
-      //{/* Vis bilde hvis cover_i-feltet er tilgjengelig */}
     </div>
   );
 };
@@ -16,8 +15,6 @@ const BookCard = ({ book }) => {
 export default BookCard;
 
 
-//{book.cover_i && <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`} alt="Book Cover" />}
-//{book.cover_i && <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`} alt="Book Cover" />}
 
 
 
@@ -26,15 +23,5 @@ export default BookCard;
 
 
 
-//const MovieCard = ({ book }) => {
-//  return (
- //   <div className="book">
-  //    <h2>{book.title}</h2>
-  //    <p>Author: {book.author}</p>
-   //   <p>Year: {book.year}</p>
-   //   {/* Add more book details as needed */}
-    //</div>
-  //);
-//};
 
 
