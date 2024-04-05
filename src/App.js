d00854cbea7dd63fc8c7adf45432c7f41a8fb9d2
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-04-05 10:58:15 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import React, { useState, useEffect } from 'react';
-
 import MySearchBar from './components/MySearchBar';
 import MySearchResults from './components/MySearchResults';
 import BookCard from './components/BookCard';
