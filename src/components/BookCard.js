54e9cb9de62d0c0e9d4a4d131f7f24872fea6de9
vetleenb <122876020+vetleenb@users.noreply.github.com>
2024-04-05 10:49:06 +0200

    Update BookCard.js

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -6,10 +6,10 @@ const BookCard = ({ book }) => {
       <h2>{book.title}</h2>
       <p>Forfatter: {book.author_name ? book.author_name.join(', ') : 'Ian Fleming'}</p>
       <p>År: {book.first_publish_year}</p>
+  {book.cover_i && <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`} alt="Book Cover" />}
     </div>
   );
 };
-//{book.cover_i && <img src={`https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg`} alt="Book Cover" />}
 export default BookCard;
 
 
